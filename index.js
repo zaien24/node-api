ddfe738b03f536f370cfa0385ab0b91b6631def4
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-12 20:21:13 +0900

    미들웨어 만들기

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
 const app = express();
 
+
+function logger(req, res, next) {
+    console.log('i am logger');
+    next();
+}
+
+//MIDDELEWARE ADD
+app.use(logger);
 
 app.listen(3000, function() {
     console.log('SERVER IS RUNNING ')
