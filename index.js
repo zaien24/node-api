1b5dba553d6aca5f5e4860bff9ac6b6035862066
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-12 20:23:04 +0900

    미들웨어 실행 순서

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ function logger(req, res, next) {
     next();
 }
 
+function logger2(req, res, next) {
+    console.log('i am logger2');
+    next();
+}
+
 //MIDDELEWARE ADD
 app.use(logger);
+app.use(logger2);
+
 
 app.listen(3000, function() {
     console.log('SERVER IS RUNNING ')
