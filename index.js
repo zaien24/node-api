9710e7c1bbe13cafe502cd4e92154f1075f866a8
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-13 22:29:44 +0900

    테스트 환경 개선

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ var bodyParser = require('body-parser');
 var user = require('./api/user/index.js'); // or ./api/user
 
 
+if (process.env.NODE_ENV !== 'test')  {
+  app.use(morgan('dev'));
+}
+
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.use(morgan('dev'));
+
 app.use('/users', user);
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-
 
 module.exports = app;
