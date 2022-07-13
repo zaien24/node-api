54d443d52e9d7ff3e0a3f0654ac0bf9dc2dfc6cf
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-13 21:08:46 +0900

    사용자 추가 API 실패 시

--- index.spec.js
+++ index.spec.js
@@ -98,5 +98,21 @@ describe('POST /users', () => {
         it('입력한 name을 반환한다', () => {          
             body.should.have.property('name', name);
         });
-    })
+    });
+    describe('실패시', () => {
+        it('name 파라미터 누락시 400을 반환한다', (done) => {
+            request(app)
+                .post('/users')
+                .send({})
+                .expect(400)
+                .end(done)
+        });
+        it('name이 중복될 경우 409을 반환한다', done => {
+            request(app)
+                .post('/users')
+                .send({name: 'alice'})
+                .expect(409)
+                .end(done)
+        });
+    });
 })
